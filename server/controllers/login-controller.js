3abf3f0bb4ec790c74ed2e262da548f72c522ee1
wsLord <priyanshusmvs@gmail.com>
2021-10-14 14:28:11 +0530

    Added Backend Dev Structure

---
+++ server/controllers/login-controller.js
@@ -0,0 +1,3 @@
+const express = require('express');
+
+//TEMP_CODE
--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const mainRouter = require('./routes/mainRouter');
+
 const port = 5000;
 
-app.get("/", (req, res, next) => {
-	res.send("Testing!");
-});
+app.use('/', mainRouter);
 
 app.listen(port, () => {
 	console.log("Server Started!");
