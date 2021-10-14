610626b7e091d2ddce1461f953daa8909ab1a65e
Nilotpal Das <neelot3pal@gmail.com>
2021-10-14 17:02:44 +0530

    more controller and router set up

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -3,5 +3,5 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 router.get("/",homeController.home);
-
+router.use("/users",require('./users'));
 module.exports = router;
