540972a7472d8fa0ceb7503f1eb83d598ef40077
wsLord <priyanshusmvs@gmail.com>
2021-10-15 14:01:26 +0530

    Added ENV support

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
 const app = express();
+
 const mainRouter = require('./routes/mainRouter');
-const port = 5000;
+
 const db = require('./config/mongoose')
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
 app.use('/', mainRouter);
 
 app.listen(port, (err) => {
