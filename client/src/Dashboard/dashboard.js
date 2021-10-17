75de0e2b90a1bede93e54a7d2e652a5ab7da3b53
Sanskar Jain <sanskar94511@gmail.com>
2021-10-17 17:50:22 +0530

    Working on dashboard

--- client/src/Dashboard/dashboard.js
+++ client/src/Dashboard/dashboard.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Header from './Header'
 
 export default function dashboard() {
     return (
         <div>
-            <Header />
+            hello dashboard
         </div>
     )
 }
