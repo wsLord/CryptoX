efae6d5d0390b28d221b7ce0bb69c23a0a045ec2
Nilotpal Das <neelot3pal@gmail.com>
2021-10-19 14:36:59 +0530

    sell limit order place function made

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -167,10 +167,38 @@ module.exports.buyLimit=async (req,res)=>{
     try{
         let newRequest = await BuyRequest.create({
             coinId:req.body.coinId,
-            form:user.walletId,
+            from:user.walletId,
             quantity:req.body.quantity,
             mode:'1',
             maxPrice:maxpri.toString(),
+            portfolioId:user.portfolioId
+        });
+
+       
+        return res.redirect('back');
+    }
+    catch(err) {
+        console.log('error',err);
+        res.redirect('back');
+    }
+}
+
+module.exports.sellLimit=async (req,res)=>{
+    if(!req.userData){
+        res.redirect('back');
+    }
+    let user = await User.findById(req.userData);
+    if(!user){
+        res.redirect('back');
+    }
+    let minpri=BigInt(req.body.minPrice*10000000);
+    try{
+        let newRequest = await SellRequest.create({
+            coinId:req.body.coinId,
+            from:user.walletId,
+            quantity:req.body.quantity,
+            mode:'1',
+            minPrice:minpri.toString(),
             portfolioId:user.portfolioId
         });
 
