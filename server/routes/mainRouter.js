114d06ce4ce8c450e6a1e76c7364e8b63d9d5a32
Nilotpal Das <neelot3pal@gmail.com>
2021-10-20 20:36:41 +0530

    verification router made

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 const usersRouter = require('./usersRouter');
 const transactionRouter=require('./transactionRouter');
-
+const verificationRouter=require('./verificationRouter');
 const homeController = require('../controllers/homeController');
-const verification = require('../controllers/verificationController');
+// const verification = require('../controllers/verificationController');
 const referralController = require('../controllers/referralController');
 // const forgotPasswordController = require('../controllers/forgotPasswordController');
 
 router.get("/", homeController.home);
 router.use("/user", usersRouter);
-router.use("/verify", verification);
+router.use("/verify", require('./verificationRouter'));
 router.use("/referral", referralController);
 router.use("/transaction", transactionRouter);
 // router.use("/forgotpassword", forgotPasswordController);
