903097e9584b2aae5e695d753b1826dd41227b61
nilotpal das <55799476+god-ctrl@users.noreply.github.com>
2021-12-26 11:35:39 +0530

    made schemas for sellLimit Transaction and buyLimit Transaction

---
+++ server/models/transactions/buyLimit.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const buyLimitSchema = new mongoose.Schema(
+	{
+		wallet: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Wallet",
+		},
+		coinid: {
+			type: String,
+			required: true,
+		},
+		amount: {
+			type: String,
+			required: true,
+		},
+        mode: {
+            type:String,//1 means limit order from the main market and 2 means limit order from the user in the app itself
+            required :true
+            
+            
+        },
+        maxPrice: {
+            type:String,
+            required :true
+        },
+		quantity: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			required: true,
+			enum: ["SUCCESS", "PENDING", "FAILED"],
+		},
+		statusMessage: {
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("buyLimit", buyLimitSchema);
