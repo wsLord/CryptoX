8d311b561485ab7529e6d6175219e1322024f7fd
wsLord <priyanshusmvs@gmail.com>
2021-12-27 11:44:53 +0530

    Receive Coin done

--- client/src/Exchange/Receive.js
+++ client/src/Exchange/Receive.js
@@ -1,23 +1,55 @@
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 import QRCode from "qrcode.react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const Receive = () => {
-	const data = { mail: "Sanskar@gmail.com", link: "http://localhost:3000/" };
+import AuthContext from "../store/authContext";
 
-	const [copy, setcopy] = useState("fa fa-clone fs-4");
+const Receive = ({ onError }) => {
+	const ctx = useContext(AuthContext);
+
+	const [email, setEmail] = useState("...");
+	const [copy, setCopy] = useState("fa fa-clone fs-4");
+
+	useEffect(() => {
+		// Getting Email of User
+		const fetchData = async () => {
+			try {
+				const { data } = await axios.post(
+					`${process.env.REACT_APP_SERVER_URL}/user/data`,
+					{
+						email: true,
+					},
+					{
+						headers: {
+							Authorization: "Bearer " + ctx.token,
+						},
+					}
+				);
+
+				setEmail(data.email);
+			} catch (err) {
+				// Error fetching User Info
+				console.log(err.response);
+				// onError(err.response.data.message);
+			}
+		};
+
+		fetchData();
+	}, [ctx]);
+
 	const copyHandler = () => {
-		setcopy("fa fa-check-circle-o text-success fs-4");
+		setCopy("fa fa-check-circle-o text-success fs-4");
 		setTimeout(() => {
-			setcopy("fa fa-clone fs-4");
+			setCopy("fa fa-clone fs-4");
 		}, 3000);
 	};
-	
+
 	return (
 		<div>
 			<div className="card m-4">
 				<div className="card-body">
-					<QRCode className="m-3" value={data.link} fgColor="#d4af37" />
+					<QRCode className="m-3" value={email} fgColor="#d4af37" />
 					<hr />
 					<div className="text-start">
 						<h4>Email ID</h4>
@@ -25,18 +57,16 @@ const Receive = () => {
 							<input
 								type="text"
 								className="form-control fs-4 rounded-0 text-secondary border-white bg-white"
-								value={data.mail}
+								value={email}
 								readOnly
 							/>
-							<CopyToClipboard text={data.mail} onCopy={copyHandler}>
+							<CopyToClipboard text={email} onCopy={copyHandler}>
 								<button className="btn btn-outline-white rounded-0 border-white">
 									<i className={copy}></i>
 								</button>
 							</CopyToClipboard>
 						</div>
-						<p className="text-primary">
-							*Share this registered mail ID to receive payment
-						</p>
+						<p className="text-primary">*Share this Registered Email ID to receive coins</p>
 					</div>
 				</div>
 			</div>
