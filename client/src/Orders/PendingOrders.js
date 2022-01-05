3637aa92856dbc95ba3bd3c8b26ab1af363049b4
wsLord <priyanshusmvs@gmail.com>
2022-01-05 22:58:56 +0530

    Minor change... Bbye

--- client/src/Orders/PendingOrders.js
+++ client/src/Orders/PendingOrders.js
@@ -19,16 +19,7 @@ const PendingOrders = ({ pendingOrderList }) => {
 				<tbody>
 					{pendingOrderList.map((element) => {
 						return (
-							<tr
-								onClick={() => {
-									history.push({
-										pathname: "/transactions/" + element.nextPath,
-										state: {
-											tid: element.id,
-										},
-									});
-								}}
-							>
+							<tr>
 								<td className="h5">{element.type}</td>
 								<td>{new Date(element.date).toString().slice(0, -31)}</td>
 								<td>{element.coinID}</td>
@@ -73,7 +64,19 @@ const PendingOrders = ({ pendingOrderList }) => {
 													<button type="button" class="btn btn-light" data-bs-dismiss="modal">
 														<i class="fa fa-times text-danger fs-4"></i>
 													</button>
-													<button type="button" class="btn btn-light">
+													<button
+														type="button"
+														class="btn btn-light"
+														data-bs-dismiss="modal"
+														onClick={() => {
+															history.push({
+																pathname: "/transactions/" + element.nextPath,
+																state: {
+																	tid: element.id,
+																},
+															});
+														}}
+													>
 														<i class="fa fa-check text-success fs-4"></i>
 													</button>
 												</div>
